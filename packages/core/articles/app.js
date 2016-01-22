24b31947f6d63912eb0bed2f30e98407eea2f176
Grant Campanelli <grantcampanelli@gmail.com>
2016-01-21 17:00:01 -0800

    MAJOR CHANGE - created custom event package

--- packages/core/articles/app.js
+++ packages/core/articles/app.js
@@ -20,21 +20,21 @@ Articles.register(function(app, auth, database, circles, swagger) {
 
   
   //We are adding a link to the main menu for all authenticated users
-  Articles.menus.add({
-    'roles': ['authenticated'],
-    'title': 'Events',
-    'link': 'all articles'
-  });
-  Articles.menus.add({
-    'roles': ['authenticated'],
-    'title': 'Create New Event',
-    'link': 'create article'
-  });
-
-  Articles.events.defaultData({
-    type: 'post',
-    subtype: 'article'
-  });
+  //Articles.menus.add({
+  //  'roles': ['authenticated'],
+  //  'title': 'Events',
+  //  'link': 'all articles'
+  //});
+  //Articles.menus.add({
+  //  'roles': ['authenticated'],
+  //  'title': 'Create New Event',
+  //  'link': 'create article'
+  //});
+  //
+  //Articles.events.defaultData({
+  //  type: 'post',
+  //  subtype: 'article'
+  //});
 
 
   /*
